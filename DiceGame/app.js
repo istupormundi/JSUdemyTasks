97fccfe0e05c9e58f699e8422837d928e1ad6492
istupormundi <dasha.prikhodko@gmail.com>
2020-03-11 19:27:16 +0300

    review part 2

--- DiceGame/app.js
+++ DiceGame/app.js
@@ -1,5 +1,4 @@
 /**************** INITIALIZATIONS **********************/
-
 let scores, roundScore, activePlayer, dice, diceExt, gamePlaying; 
 
 const diceDOM = document.querySelector('#dice-0');
@@ -12,64 +11,63 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     const previousDice = dice;
     const previousDiceExt = diceExt;
 
-    if (gamePlaying){     
-        
-        disableWinnerScore();
-        //1. Roll the dice == random number 
-        dice = Math.floor(Math.random()*6) + 1;
-        diceExt = Math.floor(Math.random()*6) + 1;
-        
-        const total = dice + diceExt;
+    if (!gamePlaying) return;
+    -
+    disableWinnerScore();
+    //1. Roll the dice == random number 
+    dice = Math.floor(Math.random()*6) + 1;
+    diceExt = Math.floor(Math.random()*6) + 1;
     
-        //2.Display the result
-        diceDOM.style.display = 'block';
-        diceDOMExt.style.display = 'block';
-        diceDOM.src = 'dice-' +  dice + '.png';
-        diceDOMExt.src = 'dice-' +  diceExt + '.png';
-    
-        //3. update the round score IF the rolled number is not 1
-        if (dice !== 1 && diceExt !== 1 && (!checkIfSixAppearedTwice(dice, diceExt, previousDice, previousDiceExt))){
-            roundScore += total;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        }
-        else{
-            loseScores(activePlayer);
-            nextPlayer();
-        }   
+    const total = dice + diceExt;
+
+    //2.Display the result
+    diceDOM.style.display = 'block';
+    diceDOMExt.style.display = 'block';
+    diceDOM.src = 'dice-' +  dice + '.png';
+    diceDOMExt.src = 'dice-' +  diceExt + '.png';
+
+    //3. update the round score IF the rolled number is not 1
+    if (dice !== 1 && diceExt !== 1 && (!checkIfSixAppearedTwice(dice, diceExt, previousDice, previousDiceExt))){
+        roundScore += total;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
     }
+    else{
+        loseScores(activePlayer);
+        nextPlayer();
+    }   
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    if (gamePlaying){
-        //Add current score to global score
-        scores[activePlayer] += roundScore;
+    if (!gamePlaying) return;
+
+    //Add current score to global score
+    scores[activePlayer] += roundScore;
+
+    //update UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
-        //update UI
-        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-        
-        let input = document.querySelector('.final-score').value;
-        
-        //if input = 0, play till 100
-        if (input == "" || input == 0){
-            input = 100;
-        }
+    let input = document.querySelector('.final-score').value;
     
-        //check if playe won the game
-        if (scores[activePlayer] >= input){
-            document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
-            diceDOM.style.display = 'none';
-            diceDOMExt.style.display = 'none';
-            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-        
-            //disable 'ROLL DICE' and 'HOLD' buttons via STATE VARIABLE
-            gamePlaying = false;
-        }
-        else{
-            disableWinnerScore();
-            nextPlayer();   
-        }   
+    //if input = 0, play till 100
+    if (input == "" || input == 0){
+        input = 100;
     }
+
+    //check if playe won the game
+    if (scores[activePlayer] >= input){
+        document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
+        diceDOM.style.display = 'none';
+        diceDOMExt.style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    
+        //disable 'ROLL DICE' and 'HOLD' buttons via STATE VARIABLE
+        gamePlaying = false;
+    }
+    else{
+        disableWinnerScore();
+        nextPlayer();   
+    }   
 });
 
 document.querySelector('.btn-new').addEventListener('click', initialize);
@@ -110,6 +108,7 @@ function initialize(){
     activePlayer = 0;
     gamePlaying = true;
     
+    //TODO: beautify
     //hide dice picture by default
     diceDOM.style.display = 'none';
     diceDOMExt.style.display = 'none';
